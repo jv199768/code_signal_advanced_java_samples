d3c977ed8dfbd0854c662e4d5e370f47d54a85f3
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 23:22:12 -0400

    Create columnTraverse2.java

---
+++ columnTraverse2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int[] columnTraverse(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        String direction = "up";
+        int row = rows - 1;
+        int col = cols - 1;
+        int[] output = new int[rows * cols];
+        int index = 0;
+    }
+}
