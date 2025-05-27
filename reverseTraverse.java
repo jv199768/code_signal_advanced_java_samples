3f26875be0ed018d94f3bb1463e1803a5c243206
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 23:23:00 -0400

    Create reverseTraverse.java

---
+++ reverseTraverse.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int[] reverseTraverse(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] output = new int[rows * cols];
+        int index = 0;
+
+        for (int row = rows - 1; row >= 0; --row) {
+            for (int col = cols - 1; col >= 0; --col) {
+                output[index++] = matrix[row][col];
+            }
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+        };
+
+        int[] result = reverseTraverse(matrix);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
