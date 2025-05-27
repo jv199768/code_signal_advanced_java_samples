30e0c865627264a74aad360a51a21a6845c7ccb0
Jaivir Baweja <154536624+jv199768@users.noreply.github.com>
2025-05-26 23:24:20 -0400

    Create findPositions.java

---
+++ findPositions.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static List<int[]> findPositions(char[][] board) {
+        List<int[]> positions = new ArrayList<>();
+
+        int rows = board.length;
+        int cols = board[0].length; 
+        // Further exploration follows
